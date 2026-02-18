357eafbd145c2664604c025395ef8d062927ef68
dharmesh1308 <dharmesh132005@gmail.com>
2026-02-18 09:05:39 +0530

    completed portfolio

---
+++ src/assets/pages/Home.jsx
@@ -0,0 +1,34 @@
+import  Navbar  from "../../Components/Navbar";
+import { ThemeToggle }  from "../../Components/ThemeToggle";
+import { StarBackground } from "../../Components/StarBackground";
+import { HeroSection } from "../../Components/HeroSection";
+import { AboutSection } from "../../Components/AboutSection";
+import { SkillsSection } from "../../Components/SkillsSection";
+import   ProjectSection  from "../../Components/ProjectSection";
+ import { ContactSection } from "../../Components/ContactSection";
+ import { Footer } from "../../Components/Footer";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      
+      <ThemeToggle />
+
+      <StarBackground />
+
+      
+      <Navbar />
+      
+      <main>
+        <HeroSection />
+        <AboutSection />
+         <SkillsSection/>
+         <ProjectSection />
+         <ContactSection/>
+      </main>
+
+      
+       <Footer/>
+    </div>
+  );
+};
